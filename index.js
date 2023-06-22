695bd769c1413f855d775635c8377069fcd63968
Ananya Budhauliya <87610762+ananyajayant@users.noreply.github.com>
2023-06-22 16:26:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "google./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
